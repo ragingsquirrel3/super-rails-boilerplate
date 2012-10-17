3b9a47b6b26894e703be395cf2b3249fb2e1d3e7
Travis Sheppard <travissheppard.1988@gmail.com>
2012-10-17 18:27:49 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperRailsBoilerplate::Application.config.secret_token = '16f763da538db7477ab76875dc52c5cc59f0f50f241a8a04a9abdd09d149ecfa73f4116870a82db13f5dca565dcf1a8523e78c9d7f5dc16d9f6addd051c027ad'
